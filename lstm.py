864df1e77c235eeb799d6a663d8ebaa1ef94278f
jkat <jasper@openriver.com>
2024-10-27 15:50:01 -0400

    fixed spelling error

--- lstm.py
+++ lstm.py
@@ -93,7 +93,7 @@ def build_lstm_model(input_shape):
     model = Sequential()
     model.add(LSTM(units=50, return_sequences=True, input_shape=input_shape))
     model.add(Dropout(0.2))
-    model.add(LSTM(unites=50))
+    model.add(LSTM(units=50))
     model.add(Dropout(0.2))
     model.add(Dense(units=1))
     model.compile(optimizer="adam", loss="mean_squared_error")
