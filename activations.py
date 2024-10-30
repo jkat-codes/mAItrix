a79dd16c88f9d3928bf4372d09c567444ca853a0
jkat <jasper@openriver.com>
2024-10-30 17:06:24 -0400

    added more activations

--- activations.py
+++ activations.py
@@ -1,28 +1,36 @@
 import numpy as np
 
-def sigmoid(Z): 
+
+def sigmoid(Z):
     return 1 / (1 + np.exp(-Z))
 
-def softmax(Z): 
+
+def softmax(Z):
     Z_shifted = Z - np.max(Z, axis=0, keepdims=True)
     exp_vals = np.exp(Z_shifted)
     return exp_vals / np.sum(exp_vals, axis=0, keepdims=True)
 
-def relu(Z): 
+
+def relu(Z):
     return np.maximum(0, Z)
 
-def tanh(x): 
+
+def tanh(x):
     return np.tanh(x)
 
-def leaky_relu(Z, cache): 
+
+def leaky_relu(Z, cache):
     return np.maximum(0.01 * Z, Z)
+
 
 ## 0.01 in the function above and below can be changed
 
-def deriv_leaky_relu(Z, cache): 
-    return np.where(Z > 0, 1, 0.01)
 
-def deriv_sigmoid(dA, cache): 
+# def deriv_leaky_relu(Z, cache):
+#     return np.where(Z > 0, 1, 0.01)
+
+
+def deriv_sigmoid(dA, cache):
     Z = cache
 
     s = 1 / (1 + np.exp(-Z))
@@ -30,14 +38,20 @@ def deriv_sigmoid(dA, cache):
 
     return dZ
 
-def deriv_relu(Z): 
+
+def deriv_relu(Z):
     return Z > 0
 
 
-def deriv_tanh(x): 
-    return (1 - np.power(x, 2))
+def deriv_leaky_relu(A, alpha=0.01):
+    return np.where(A > 0, 1, alpha)
 
-def one_hot(Y): 
+
+def deriv_tanh(x):
+    return 1 - np.power(x, 2)
+
+
+def one_hot(Y):
     one_hot_Y = np.zeros((Y.size, Y.max() + 1))
     one_hot_Y[np.arange(Y.size), Y] = 1
     one_hot_Y = one_hot_Y.T
